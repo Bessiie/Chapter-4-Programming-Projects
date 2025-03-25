c532036ff4325f35a45e56d2c8e760128985ad85
Jesse zheng <BessiieBoi@outlook.com>
2025-03-25 19:44:59 -0400

    Version 1

---
+++ Chapter4Projexts/Chapter4Projects.cpp
@@ -0,0 +1,37 @@
+//Write a program that asks the user to enter the month (letting the user enter an integer in the range of 1 through 12) and the year.
+//The program should then display the number of days in that month.
+//Determine whether the year is divisible by 100. If it is, then it is a leap year if and only if it is divisible by 400.
+//If the year is not divisible by 100, then it is a leap year if and only if it is divisible by 4.
+
+#include <iostream>
+
+//Checks if leap year or not
+bool isLeapYear(int year) {
+	return (year % 400 == 0 || year % 100 != 0 && year % 4 == 0);
+}
+
+//Main function for determining days in a month
+int main() {
+	
+	//Variables
+	int month, year;
+	
+	//Array of days in a month
+	int daysInAMonth[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+
+	//Asks user for month and year value
+		std::cout << "Enter a month (1-12):\n";
+		std::cin >> month;
+		std::cout << "Enter a year:\n";
+		std::cin >> year;
+
+	//Checks if month is Feburary and if it is a leap year
+		if (month == 2 && isLeapYear(year) == true) {
+			std::cout << daysInAMonth[month - 1] + 1 << std::endl;
+		}
+	//if not leap year then output not leap year values
+		else {
+			std::cout << daysInAMonth[month - 1] << std::endl;
+		}
+	return 0;
+}
